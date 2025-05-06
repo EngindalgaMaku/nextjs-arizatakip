d7d6645e2018d16298dbcd5fac9023ae3fc4405d
EngindalgaMaku <2430131010@ogr.mehmetakif.edu.tr>
2025-05-06 23:56:03 +0300

    atsis 1.6

--- src/app/dashboard/dallar/page.tsx
+++ src/app/dashboard/dallar/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchDallar, createDal, updateDal, deleteDal, fetchBranchesForSelect, fetchDallarByBranch } from '@/actions/dalActions';
